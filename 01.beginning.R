ff64fe664793070b65b740ff27163068c26679f6
carlospacca <146854961+carlospacca@users.noreply.github.com>
2023-10-03 17:10:22 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,20 @@
+# Here i can write anything i want!!! 42 is the meaning of life universe and all!
+
+# R as a calculator
+2+3
+
+# Assign to an object
+zima <- 2 + 3
+zima
+
+duccio <- 5+3
+duccio
+
+zima*duccio
+
+final <- zima*duccio
+final
+
+final^2
+
+
